716d1de7fc92202fd43a23549128220983b167b1
Paolo Losi <paolo.losi@gmail.com>
2012-01-19 11:45:27 +0100

    test macro: init_problem

--- tests/test.h
+++ tests/test.h
@@ -5,4 +5,12 @@
 
 #define test_header() printf(">>> test: %s\n", __func__)
 
+#define init_problem(p, vectors, labels)                                       \
+    do {                                                                       \
+        (p)->vectors = vectors;                                                \
+        (p)->labels = labels;                                                  \
+        (p)->n_samples  = sizeof(labels)  / sizeof(*labels);                   \
+        (p)->n_features = sizeof(vectors) / sizeof(*vectors) / (p)->n_samples; \
+    } while(0);
+
 #endif
--- tests/test_tree.c
+++ tests/test_tree.c
@@ -1,23 +1,20 @@
 #include "rt.h"
 #include "test.h"
 
-static double test_vecs[] = { 1., 3.,
-                              2., 4.,
-                              1., 6. };
-
-static double test_labs[] = { 2., 3., 4. };
-
-static rt_problem prob = (rt_problem) {
-   .vectors = test_vecs,
-   .labels  = test_labs,
-   .n_features = 3,
-   .n_samples  = 2,
-};
 
 void test_leaf() {
     test_header();
+
+    rt_problem prob;
+    double vectors[] = { 1., 3.,
+                         2., 4.,
+                         1., 6.};
+    double labels[] = { 2., 3., 4. };
+
+    init_problem(&prob, vectors, labels);
     rt_print_problem(&prob);
 }
+
 
 int main() {
     test_leaf();
